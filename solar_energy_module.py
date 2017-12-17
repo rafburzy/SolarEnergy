5d67c793a5cb8bd643cf4a8ef0b5620332a1a9d3
rafburzy <rafburzy@gmail.com>
2017-12-17 18:48:34 +0100

    added pv module data

--- solar_energy_module.py
+++ solar_energy_module.py
@@ -11,7 +11,14 @@ def junctionVoltage(Na, Nd, ni, T):
 
     Inputs:
     ------------------
-
+    Na: float
+        Acceptor carriers concentration [1/cm3]
+    Nd: float
+        Donor carriers concentration [1/cm3]
+    ni: float
+        Intrinsic carrier concentration [1/cm3]
+    T: float
+        Temperature [K]
 
     Returns:
     ------------------
@@ -19,3 +26,14 @@ def junctionVoltage(Na, Nd, ni, T):
         Voltage built across the p-n junction in [V] '''
 
     return k*T/q * np.log(Na*Nd/ni**2)
+
+# add function showing Planck's law for black body at 5800K
+
+
+###########################################################
+# PV Systems functions
+###########################################################
+
+def pv_efficiency(Ump, Imp, H, W):
+    '''Placeholder for help'''
+    return Ump*Imp/(H*W)/1000
